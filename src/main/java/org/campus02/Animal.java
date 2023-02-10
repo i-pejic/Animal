6043ca0cf86c6feb526db6920d20beef2c1549f4
ipejic <s52456@edu.campus02.at>
2023-02-10 09:57:35 +0100

    Initial commit

---
+++ src/main/java/org/campus02/Animal.java
@@ -0,0 +1,8 @@
+package org.campus02;
+
+public class Animal {
+    private String name;
+    private int age;
+    private String species;
+    private String animalLoud;
+}
---
+++ src/test/java/org/campus02/AnimalTest.java
@@ -0,0 +1,7 @@
+package org.campus02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimalTest {
+
+}
